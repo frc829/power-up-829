a8594e826340c2157526f6c68a8f33e0e9d49ed5
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-01-06 17:51:14 -0500

    Removed unnecessary comments in Robot

--- src/main/java/com/digitalgoats/robot/Robot.java
+++ src/main/java/com/digitalgoats/robot/Robot.java
@@ -7,37 +7,30 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class Robot extends IterativeRobot {
 
-  // Called once the robot is powered on, use this to setup motors
   @Override
   public void robotInit() {
   }
 
-  // Called once the robot is disabled
   @Override
   public void disabledInit() {
   }
 
-  // Called once the robot enters autonomous period
   @Override
   public void autonomousInit() {
   }
 
-  // Called once the robot enter teleoperated period
   @Override
   public void teleopInit() {
   }
 
-  // Called periodically while robot is disabled
   @Override
   public void disabledPeriodic() {
   }
 
-  // Called periodically while robot is in autonomous period
   @Override
   public void autonomousPeriodic() {
   }
 
-  // Called periodically while robot is in teleoperated period
   @Override
   public void teleopPeriodic() {
   }
