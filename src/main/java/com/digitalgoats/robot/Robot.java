c41d903fcff920aef8b5eadd68bd3c72387883bf
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-01-06 16:29:16 -0500

    Added IGoatSystem and added other robot methods

--- src/main/java/com/digitalgoats/robot/Robot.java
+++ src/main/java/com/digitalgoats/robot/Robot.java
@@ -2,28 +2,42 @@ package com.digitalgoats.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
+/**
+ * The main robot class for FIRST POWER UP
+ */
 public class Robot extends IterativeRobot {
 
+  // Called once the robot is powered on, use this to setup motors
   @Override
   public void robotInit() {
   }
 
+  // Called once the robot is disabled
   @Override
   public void disabledInit() {
   }
 
+  // Called once the robot enters autonomous period
   @Override
   public void autonomousInit() {
   }
 
+  // Called once the robot enter teleoperated period
   @Override
   public void teleopInit() {
   }
 
+  // Called periodically while robot is disabled
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  // Called periodically while robot is in autonomous period
   @Override
   public void autonomousPeriodic() {
   }
 
+  // Called periodically while robot is in teleoperated period
   @Override
   public void teleopPeriodic() {
   }
---
+++ src/main/java/com/digitalgoats/systems/IGoatSystem.java
@@ -0,0 +1,39 @@
+package com.digitalgoats.systems;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ * The interface that represents a particular system for the robot
+ * @author Blake
+ */
+public interface IGoatSystem {
+
+  /**
+   * System update periodically called while robot is disabled
+   */
+  public void disabledUpdate();
+
+  /**
+   * System update method called periodically while robot is in teleoperated period
+   * @param driver
+   *  The controller used by the primary driver
+   * @param operator
+   *  The controller used by the systems operator
+   */
+  public void updateSystem(Joystick driver, Joystick operator);
+
+  /**
+   * SmartDashboard update method called periodically while robot is in teleoperated period
+   */
+  public void updateSmartDashboard();
+
+  /**
+   * Find the system name for the particular GoatSystem
+   * @return
+   *  The current GoatSystem's name
+   */
+  default public String getSystemName() {
+    return "No System Name Specified";
+  };
+
+}
