92393305face8d7fd69586e15ba4d25492478993
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-01-09 21:10:33 -0500

    Added compressor to systems

--- src/main/java/com/digitalgoats/systems/Systems.java
+++ src/main/java/com/digitalgoats/systems/Systems.java
@@ -2,6 +2,7 @@ package com.digitalgoats.systems;
 
 import com.digitalgoats.util.LogitechF310;
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import java.util.ArrayList;
 
@@ -12,6 +13,9 @@ public class Systems implements IGoatSystem {
 
   // Objects
   public AHRS navx;
+  public Compressor compressor;
+
+  // Systems
   public ArrayList<IGoatSystem> systems;
   public Drive drive;
 
@@ -20,17 +24,26 @@ public class Systems implements IGoatSystem {
    */
   public Systems() {
 
-    systems = new ArrayList<IGoatSystem>();
+    // Setup Objects
     navx = new AHRS(Port.kMXP);
+    compressor = new Compressor(SystemMap.DRIVE_PCM.getValue());
+
+    // Setup Systems
+    systems = new ArrayList<IGoatSystem>();
     drive = new Drive(navx);
 
     // Add Individual Systems
     systems.add(drive);
 
+    // Start Compressor
+    compressor.clearAllPCMStickyFaults();
+    compressor.start();
+
   }
 
   @Override
   public void disabledUpdateSystem() {
+    compressor.stop();
     for (IGoatSystem system : systems) {
       system.disabledUpdateSystem();
     }
