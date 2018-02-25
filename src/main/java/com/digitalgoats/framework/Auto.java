f0e5779c279bf507b61d34249165d49fbcb466ae
Blake Johnson <blake.k.johnson.4@gmail.com>
2018-02-24 22:02:06 -0500

    Added comments to framework classes

--- src/main/java/com/digitalgoats/framework/Auto.java
+++ src/main/java/com/digitalgoats/framework/Auto.java
@@ -3,18 +3,38 @@ package com.digitalgoats.framework;
 import com.digitalgoats.robot.SystemGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
+/**
+ * The super class for Autonomous modes
+ */
 public abstract class Auto {
 
+  /** The current step for autonomous */
   private int step;
+  /** The name for the autonomous */
   private String name;
+  /** The systems available during autonomous */
   private SystemGroup systems;
 
+  /**
+   * Create an autonomous
+   * @param name
+   *  The name of the autonomous
+   * @param systems
+   *  The systems available
+   */
   public Auto(String name, SystemGroup systems) {
     this.setStep(0);
     this.setName(name);
     this.setSystems(systems);
   }
 
+  /**
+   * Add the auto to the chooser for Shuffleboard
+   * @param chooser
+   *  The SendableChooser for auto selection
+   * @param isDefault
+   *  Should the auto be selected by default
+   */
   public void addToDashboard(SendableChooser<String> chooser, boolean isDefault) {
     if (isDefault) {
       chooser.addDefault(this.getName(), this.getName());
@@ -24,28 +44,44 @@ public abstract class Auto {
     return;
   }
 
+  /**
+   * Go to the next step
+   */
+  public void nextStep() {
+    this.setStep(this.getStep() + 1);
+  }
+
+  /**
+   * Execute the selected autonomous
+   */
   public abstract void execute();
 
+  /** Get the current step */
   public int getStep() {
     return this.step;
   }
 
+  /** Set the current step */
   public void setStep(int step) {
     this.step = step;
   }
 
+  /** Get the autonomous's name */
   public String getName() {
     return this.name;
   }
 
+  /** Set the autonomous's name */
   public void setName(String name) {
     this.name = name;
   }
 
+  /** Get the group of systems */
   public SystemGroup getSystems() {
     return this.systems;
   }
 
+  /** Set the group of systems */
   public void setSystems(SystemGroup systems) {
     this.systems = systems;
   }
--- src/main/java/com/digitalgoats/framework/ISystem.java
+++ src/main/java/com/digitalgoats/framework/ISystem.java
@@ -2,12 +2,20 @@ package com.digitalgoats.framework;
 
 import com.digitalgoats.util.LogitechF310;
 
+/**
+ * The primary interface for robot systems
+ */
 public interface ISystem {
 
+  /** Called on autonomous initialization */
   public void autoInit();
+  /** Called to update shuffleboard */
   public void shuffleboard();
+  /** Called on teleop initialization */
   public void teleopInit();
+  /** Called during teleop period */
   public void teleopUpdate(LogitechF310 driver, LogitechF310 operator);
+  /** Called during each period of the robot */
   public void update();
 
 }
